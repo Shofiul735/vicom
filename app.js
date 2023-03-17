3da98b30c6ea2137672aa75ab0864c8f265a025c
Md. Shofiul Islam <ytshofiul@gmail.com>
2023-03-17 22:58:14 +0600

    Basic content setup implemented

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname,'public')));
 
 app.route('/')
     .get((req,res)=>{
-        return res.send('Hello')
+        return res.sendFile('index.html')
     });
 
 app.listen(PORT,()=>{
